0cfb7adfab5bd2b6c2de1a878e4f8cc298b00033
Shukrullo <94306881+Shukrullo2@users.noreply.github.com>
2024-07-22 15:01:07 +0500

    collectstatic

--- nasihat/nasihat/settings.py
+++ nasihat/nasihat/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -182,6 +183,7 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
